8540d17ee54481ff85c6fd4b6eaff38e2dafbc89
Evgeny Bayandin <5087081@gmail.com>
2024-05-26 19:50:54 +0300

    Added @Entity @Id @GeneratedValue in POJO Class Person

--- src/main/java/ru/webdev/dto/Person.java
+++ src/main/java/ru/webdev/dto/Person.java
@@ -1,20 +1,31 @@
 package ru.webdev.dto;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person {
 
+    @Id
+    @GeneratedValue
     private int id;
     private String firstname;
     private String surname;
     private String lastname;
     private LocalDate birthday;
 
+    public Person(String firstname, String surname, String lastname, java.time.LocalDate birthday) {
+        this.firstname = firstname;
+        this.surname = surname;
+        this.lastname = lastname;
+        this.birthday = birthday;
+    }
 }
