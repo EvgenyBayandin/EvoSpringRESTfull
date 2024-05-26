595fae20b614c1bd079a454a420d1a998efdf7e4
Evgeny Bayandin <5087081@gmail.com>
2024-05-26 13:00:18 +0300

    Added Person

---
+++ src/main/java/ru/webdev/Person.java
@@ -0,0 +1,20 @@
+package ru.webdev;
+
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    private int id;
+    private String firstname;
+    private String surname;
+    private String lastname;
+    private LocalDate birthday;
+
+}
