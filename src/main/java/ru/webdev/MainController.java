2cbe46e3a58d80dc62c237e9d5602f9dea6bf15b
Evgeny Bayandin <5087081@gmail.com>
2024-05-26 13:05:07 +0300

    Added @GetMapping("/person") getPerson() in MainController start project and go to http://localhost:8080/person

--- src/main/java/ru/webdev/MainController.java
+++ src/main/java/ru/webdev/MainController.java
@@ -1,10 +1,19 @@
 package ru.webdev;
 
+import java.time.LocalDate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+
+    private Person person;
+
+    @GetMapping("/person")
+    public Person getPerson() {
+        return new Person(1, "Ivanov", "Ivan", "Ivanovich", LocalDate.of(1991, 12, 12));
+    }
+
     @GetMapping
     public String hello() {
         return "Hello, world!";
