c69d80bc612c0d0e0a695bcb9617105496c948c0
Evgeny Bayandin <5087081@gmail.com>
2024-05-26 12:47:40 +0300

    Added @RestController MainController, start project and go to http://localhost:8080

---
+++ src/main/java/ru/webdev/MainController.java
@@ -0,0 +1,12 @@
+package ru.webdev;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping
+    public String hello() {
+        return "Hello, world!";
+    }
+}
