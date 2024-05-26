5e06e0e0fe46de196c52b350f9fc1dc32ba0ba6f
Evgeny Bayandin <5087081@gmail.com>
2024-05-26 21:53:54 +0300

    Updated Message, Person Added @ManyToOne @OneToMany

--- src/main/java/ru/webdev/dto/Message.java
+++ src/main/java/ru/webdev/dto/Message.java
@@ -1,8 +1,10 @@
 package ru.webdev.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,9 +23,20 @@ public class Message {
     private String text;
     private LocalDateTime time;
 
-    public Message(String title, String text, LocalDateTime time) {
+    @ManyToOne
+    @JsonIgnore // чтобы приложение не ушло в рекурсию при сериализации вывода в формат JSON
+    private Person person; // добавили связь с пользователем
+
+
+    public Message(String title, String text, LocalDateTime time, Person person) {
         this.title = title;
         this.text = text;
         this.time = time;
+        this.person = person;
+    }
+
+    public Message(String title, String text) {
+        this.title = title;
+        this.text = text;
     }
 }
--- src/main/java/ru/webdev/dto/Person.java
+++ src/main/java/ru/webdev/dto/Person.java
@@ -3,7 +3,10 @@ package ru.webdev.dto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
 import java.time.LocalDate;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,10 +25,19 @@ public class Person {
     private String lastname;
     private LocalDate birthday;
 
-    public Person(String firstname, String surname, String lastname, LocalDate birthday) {
+    @OneToMany(cascade = CascadeType.ALL) // для касскадного удаления сообщений пользователя при его удалении
+    List<Message> messages; // добавили обратную связь с сообщениями
+
+    public Person(String firstname, String surname, String lastname, LocalDate birthday, List<Message> messages) {
         this.firstname = firstname;
         this.surname = surname;
         this.lastname = lastname;
         this.birthday = birthday;
+        this.messages = messages;
+    }
+
+    // создаем метод добааления сообщения
+    public void addMessage(Message message) {
+        messages.add(message);
     }
 }
