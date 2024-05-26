d2350efc0fcc0f9ce3b85890c79010d0494144a5
Evgeny Bayandin <5087081@gmail.com>
2024-05-26 13:56:38 +0300

    Added @GetMapping("/person/{id}") getPerson() in MainController

--- src/main/java/ru/webdev/MainController.java
+++ src/main/java/ru/webdev/MainController.java
@@ -4,9 +4,11 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +32,11 @@ public class MainController {
     @GetMapping("/person")
     public Iterable<Person> getPerson() {
         return persons;
+    }
+
+    @GetMapping("/person/{id}")
+    public Optional<Person> getPersonById(@PathVariable int id) {
+        return persons.stream().filter(p -> p.getId() == id).findFirst();
     }
 
     @GetMapping
