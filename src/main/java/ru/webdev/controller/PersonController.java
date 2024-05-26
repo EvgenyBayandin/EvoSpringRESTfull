70df4d127c6e5f3ae72980697b4f5b4e7178a7a1
Evgeny Bayandin <5087081@gmail.com>
2024-05-26 20:12:29 +0300

    Updated PersonController

--- src/main/java/ru/webdev/controller/PersonController.java
+++ src/main/java/ru/webdev/controller/PersonController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,50 +16,50 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.webdev.dto.Person;
+import ru.webdev.repository.PersonRepository;
 
 @RestController
 public class PersonController {
+    /*
+    POST <http://localhost:8080/person>
+    Content-Type: application/json
 
-    private List<Person> persons = new ArrayList<>(Arrays.asList(
-            new Person(1, "Ivan", "Ivanovich", "Ivanov", LocalDate.of(1999, 2,3)),
-            new Person(2, "Петр", "Петрович", "Петров", LocalDate.of(2002, 2,2)),
-            new Person(3, "Евгений", "Васильевич", "Васин", LocalDate.of(2005, 4,8)),
-            new Person(4, "Максим", "Яковлевич", "Окопский", LocalDate.of(1978, 6,5))
-    ));
+    {
+      "firstname": "Василий",
+      "surname": "Викторович",
+      "lastname": "Петрушин",
+      "birthday": "2001-01-01"
+    }
+    */
+
+    @Autowired
+    private PersonRepository personRepository;
 
     @PostMapping("/person")
     public Person addPerson(@RequestBody Person person) {
-        persons.add(person);
+        personRepository.save(person);
         return person;
     }
 
     @PutMapping("/person/{id}")
     public ResponseEntity<Person> updatePerson(@RequestBody Person person, @PathVariable int id) {
-       int index = -1;
-        for (Person p : persons) {
-            if (p.getId() == id) {
-                index = persons.indexOf(p);
-                persons.set(index, person);
-            }
-        }
-        return index == -1
-                ? new ResponseEntity<>(addPerson(person), HttpStatus.CREATED)
-                : new ResponseEntity<>(person, HttpStatus.OK);
+       HttpStatus status = personRepository.existsById(id) ? HttpStatus.OK : HttpStatus.CREATED;
+       return new ResponseEntity<Person>(personRepository.save(person), status);
     }
 
     @GetMapping("/person")
     public Iterable<Person> getPerson() {
-        return persons;
+        return personRepository.findAll();
     }
 
     @GetMapping("/person/{id}")
     public Optional<Person> getPersonById(@PathVariable int id) {
-        return persons.stream().filter(p -> p.getId() == id).findFirst();
+        return personRepository.findById(id);
     }
 
     @DeleteMapping("/person/{id}")
     public void deletePerson(@PathVariable int id) {
-        persons.removeIf(p -> p.getId() == id);
+        personRepository.deleteById(id);
     }
 
     @GetMapping
