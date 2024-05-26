733ffbf92ed13f613b12e9754afdd55f822d495d
Evgeny Bayandin <5087081@gmail.com>
2024-05-26 12:43:48 +0300

    Created new Application Added application.properties

---
+++ src/main/java/ru/webdev/ResTfullDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.webdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfullDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTfullDemoApplication.class, args);
+    }
+
+}
