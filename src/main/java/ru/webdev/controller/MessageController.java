bccfd7926a056e88cc8ce14a2c5c912d042da9e2
Evgeny Bayandin <5087081@gmail.com>
2024-05-26 16:28:38 +0300

    Added @PutMapping("/message/{id}") updateMessage()

--- src/main/java/ru/webdev/controller/MessageController.java
+++ src/main/java/ru/webdev/controller/MessageController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.webdev.dto.Message;
@@ -38,6 +39,17 @@ public class MessageController {
         return message;
     }
 
+    @PutMapping("/message/{id}")
+    public Message updateMessage(@RequestBody Message message, @PathVariable int id) {
+        int index = messages.indexOf(message);
+        for (Message m : messages) {
+            if (m.getId() == id) {
+                index = messages.indexOf(m);
+                messages.set(index, message);
+            }
+        }
+        return message;
+    }
 
 
 }
